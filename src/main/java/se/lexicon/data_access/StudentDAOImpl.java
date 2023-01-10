8ef516359c8366cc50481d954362062db262a813
NiveSJ <niveathithavj@gmail.com>
2023-01-10 10:29:48 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/data_access/StudentDAOImpl.java
@@ -0,0 +1,31 @@
+package se.lexicon.data_access;
+
+import se.lexicon.models.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDAOImpl implements StudentDAO {
+
+    List<Student> student = new ArrayList<>();
+
+    @Override
+    public Student save(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student find(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
